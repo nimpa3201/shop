150ef2d6e7fab43c7398cd5584d3f8f5ce5fd0b4
gwongilnam <nimpa3201@naver.com>
2025-02-06 02:20:42 +0900

    폴더 생성 및 조회 구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -26,11 +26,11 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public static final int MIN_MY_PRICE =100;
+    public static final int MIN_MY_PRICE = 100;
 
     public ProductResponseDto createProduct(ProductRequestDto requestDto, User user) {
 
-        Product product = productRepository.save(new Product(requestDto,user));
+        Product product = productRepository.save(new Product(requestDto, user));
         return new ProductResponseDto(product);
 
     }
@@ -39,11 +39,11 @@ public class ProductService {
     public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
         int myprice = requestDto.getMyprice();
 
-        if(myprice< MIN_MY_PRICE){
-            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소" + MIN_MY_PRICE +"원 이상으로 설정해 주세요");
+        if (myprice < MIN_MY_PRICE) {
+            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소" + MIN_MY_PRICE + "원 이상으로 설정해 주세요");
         }
 
-        Product product = productRepository.findById(id).orElseThrow(()-> new NullPointerException("해당 상품을 찾을 수 없습니다.")
+        Product product = productRepository.findById(id).orElseThrow(() -> new NullPointerException("해당 상품을 찾을 수 없습니다.")
         );
 
         product.update(requestDto);
@@ -55,7 +55,7 @@ public class ProductService {
 
     @Transactional
     public void updateBySearch(Long id, ItemDto itemDto) {
-        Product product = productRepository.findById(id).orElseThrow(()->
+        Product product = productRepository.findById(id).orElseThrow(() ->
                 new NullPointerException("해당 상품은 존재하지 않습니다.")
         );
         product.updateByItemDto(itemDto);
@@ -74,21 +74,21 @@ public class ProductService {
 
     }
 
+    @Transactional(readOnly = true)
     public Page<ProductResponseDto> getProducts(User user, int page, int size, String sortBy, boolean isAsc) {
-        Sort.Direction direction = isAsc? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
         Sort sort = Sort.by(direction, sortBy);
-        Pageable pageable = PageRequest.of(page,size,sort);
+        Pageable pageable = PageRequest.of(page, size, sort);
 
-         UserRoleEnum userRoleEnum = user.getRole();
+        UserRoleEnum userRoleEnum = user.getRole();
 
-         Page<Product> productList;
+        Page<Product> productList;
 
-         if(userRoleEnum ==  UserRoleEnum.USER){
+        if (userRoleEnum == UserRoleEnum.USER) {
             productList = productRepository.findAllByUser(user, pageable);
-         }else{
-             productList =productRepository.findAll(pageable);
-         }
-
+        } else {
+            productList = productRepository.findAll(pageable);
+        }
 
 
         return productList.map(ProductResponseDto::new);
