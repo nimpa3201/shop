ffeb7b843dc2fb313115a090d3329a5ee7368680
gwongilnam <nimpa3201@naver.com>
2025-02-09 20:33:34 +0900

    @ControllerAdvice 적용

---
+++ src/main/java/com/sparta/myselectshop/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.sparta.myselectshop.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice  // 모든 컨트롤러에서 발생한 예외를 처리하기위해 globala 적으로 발생
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<RestApiException> handleException(IllegalArgumentException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
